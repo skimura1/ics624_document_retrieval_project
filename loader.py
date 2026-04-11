8385cc71737b831b42a17f62ca6836addc1342ba
Skyler Kimura <skyler8@hawaii.edu>
2026-04-10 15:27:29 -1000

    removed work around httpx

--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
 def load_data():
-    ds = load_dataset("microsoft/ms_marco", "v1.1", split="validation", streaming=True)
+    ds = load_dataset("microsoft/ms_marco", "v1.1", split="validation")
     return ds
