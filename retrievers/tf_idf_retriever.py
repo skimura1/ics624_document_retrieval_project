3ea7efb7b3ed2ace2a16c274fbc0b90eac104119
Skyler Kimura <skyler8@hawaii.edu>
2026-04-10 16:12:39 -1000

    fixed tf_idf returning id

--- retrievers/tf_idf_retriever.py
+++ retrievers/tf_idf_retriever.py
@@ -1,19 +1,16 @@
 from loader import load_data
+from base import BaseRetriever
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-class TFIDFRetriever:
+class TFIDFRetriever(BaseRetriever):
     def __init__(self, top_k: int = 10):
         self.vectorizer = TfidfVectorizer()
         self.top_k = top_k
         self.tf_idf_matrix = None
-        self.passages_text = None
 
     def fit(self, passages_text: list[str]):
-        # Take in the list of passages text
-        self.passages_text = passages_text
-        # Fit the vectorizer on the passages text
         self.tf_idf_matrix = self.vectorizer.fit_transform(passages_text)
 
     def query(self, query: str):
@@ -21,7 +18,7 @@ class TFIDFRetriever:
             raise ValueError("TF-IDF matrix is not fitted. Please fit the retriever first.")
         query_vector = self.vectorizer.transform([query])
         cosine_similarities = cosine_similarity(query_vector, self.tf_idf_matrix)[0]
-        top_k_indices = list(np.argsort(cosine_similarities)[::-1][:self.top_k])
+        top_k_indices = list(map(int, np.argsort(cosine_similarities)[::-1][:self.top_k]))
         return top_k_indices
 
 if __name__ == "__main__":
@@ -34,5 +31,4 @@ if __name__ == "__main__":
     retriever.fit(passages_text)
     query = "What is the capital of France?"
     top_k_indices = retriever.query(query)
-    top_k_passages = [passages_text[i] for i in top_k_indices]
-    print(top_k_passages)
+    print(top_k_indices)
